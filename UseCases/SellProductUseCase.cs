2bc024df461375fd21011588fe63e3e6ad1ffb8c
Huy Nguyen <huynguyen260398@gmail.com>
2021-11-28 19:55:20 +0700

    Implemented sell product.

---
+++ UseCases/SellProductUseCase.cs
@@ -0,0 +1,23 @@
+﻿using UseCases.DataStorePluginInterfaces;
+using UseCases.UseCaseInterfaces;
+
+namespace UseCases;
+
+public class SellProductUseCase : ISellProductUseCase
+{
+    private readonly IProductRepository productRepository;
+
+    public SellProductUseCase(IProductRepository productRepository)
+    {
+        this.productRepository = productRepository;
+    }
+
+    public void Execute(int productId, int qtyToSell)
+    {
+        var product = productRepository.GetProductById(productId);
+        if (product == null) return;
+
+        product.Quantity -= qtyToSell;
+        productRepository.UpdateProduct(product);
+    }
+}
